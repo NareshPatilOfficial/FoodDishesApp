4cc6be4b75867bdd8210ccf48137adf37ff785e1
NareshPatilOfficial <nareshpatil.nrp@gmail.com>
2023-04-10 12:35:36 +0530

    FoodDishesApp : Used veested navigation, updated babel configuration for drawer and added new screen favourite

--- App.js
+++ App.js
@@ -2,10 +2,31 @@ import { StatusBar, StyleSheet } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreens";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import MealItems from "./screens/MealItems";
 import MealOverview from "./screens/MealOverview";
+import Favourite from "./screens/FavouriteScreen";
 
 const Stack = createNativeStackNavigator();
+
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigation() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen
+        name="ManageCategories"
+        component={CategoriesScreen}
+        options={{ title: 'Manage Categories' }}
+      />
+      <Drawer.Screen
+        name="Favourite"
+        component={Favourite}
+        options={{ title: 'Favourite' }}
+      />
+    </Drawer.Navigator>
+  )
+}
 
 export default function App() {
   return (
@@ -22,9 +43,9 @@ export default function App() {
           }}
         >
           <Stack.Screen
-            name="ManageCategories"
-            component={CategoriesScreen}
-            options={{ title: 'Manage Categories' }}
+            name="ManageCategoriesRoot"
+            component={DrawerNavigation}
+            options={{ title: 'Manage Categories', headerShown: false }}
           />
           <Stack.Screen
             name="MealItem"
---
+++ screens/FavouriteScreen.js
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+function Favourite(){
+    return(
+        <View>
+            <Text>
+                Favourite
+            </Text>
+        </View>
+    )
+}
+
+export default Favourite;
